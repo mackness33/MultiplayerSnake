c6c16c332b9bfcb21b6878923fc0ccb23287747e
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2022-11-11 22:11:25 +0100

    Movement of the snake

--- lib/game/components/snake_component.dart
+++ lib/game/components/snake_component.dart
@@ -8,17 +8,22 @@ import 'package:multiplayersnake/game/models/snake_controller.dart';
 
 class SnakeComponent extends PositionComponent
     with HasGameRef<MultiplayerSnakeGame> {
-  SnakeComponent(this.screen, this.board, this.tileSize) : snake = Snake();
+  SnakeComponent(this.screen, this.board, this.tileSize)
+      : snake = Snake(),
+        speed = 1,
+        delta = 0;
 
   final Rect screen;
   final BoardController board;
   final double tileSize;
   final Snake snake;
+  final double speed;
+  double delta;
 
   @override
   Future<void>? onLoad() async {
     print('in snake loading');
-    await snake.initialize(tileSize);
+    await snake.initialize();
     await addAll(snake.body);
     print('in snake loaded');
   }
@@ -33,5 +38,15 @@ class SnakeComponent extends PositionComponent
     //   _snakeBody.first.render(canvas);
     // }
     super.render(canvas);
+  }
+
+  @override
+  void update(double dt) {
+    delta += dt;
+    if (delta > speed) {
+      snake.moveStraight();
+      delta %= speed;
+    }
+    super.update(dt);
   }
 }
--- lib/game/models/snake_controller.dart
+++ lib/game/models/snake_controller.dart
@@ -5,12 +5,19 @@ import 'package:flame/flame.dart';
 import 'package:multiplayersnake/game/components/body_component.dart';
 import 'package:multiplayersnake/game/game.dart';
 
+enum direction { up, down, right, left }
+
 class Snake {
   final List<PositionComponent> _body;
+  final double tileSize;
+  int direction;
 
-  Snake() : _body = List.empty(growable: true);
+  Snake()
+      : _body = List.empty(growable: true),
+        tileSize = 10,
+        direction = 0;
 
-  Future<void>? initialize(double tileSize) async {
+  Future<void>? initialize() async {
     final SpriteComponent head = SpriteComponent.fromImage(
       await Flame.images.load('snake/head.png'),
       size: Vector2(tileSize, tileSize),
@@ -28,6 +35,64 @@ class Snake {
 
     _body.addAll([tail, body, head]);
   }
+
+  // List<PositionComponent> _getBodyReversedListWithoutHead() {
+  //   return _getReversedListWithoutHead().toList();
+  // }
+
+  Iterable<PositionComponent> _getReversedListWithoutHead() {
+    return _body.skip(1).toList().reversed;
+  }
+
+  // List<PositionComponent> _getBodyReversedListWithoutTail() {
+  //   return _getReversedListWithoutTail().toList();
+  // }
+
+  Iterable<PositionComponent> _getReversedListWithoutTail() {
+    return _body.reversed.skip(1);
+  }
+
+  void _move(Vector2 trasition) {
+    // print(_body.reversed.toString());
+    // print(_body.reversed.iterator.toString());
+    // print(_body.reversed.iterator.current.toString());
+    final next = _body.reversed.iterator;
+    next.moveNext();
+    // print(next.current);
+    PositionComponent current = next.current;
+
+    while (next.moveNext()) {
+      current.position = next.current.position;
+      current = next.current;
+    }
+
+    // it supposed to be the head
+    current.position += trasition;
+  }
+
+  // direction: 0: top, 1: right, 2: buttom, 3: left, default: nothing
+  void _moveTo(int direction) {
+    print('moving');
+    switch (direction) {
+      case 0:
+        _move(_body.first.position + Vector2(0, tileSize));
+        break;
+      case 1:
+        _move(_body.first.position + Vector2(tileSize, 0));
+        break;
+      case 2:
+        _move(_body.first.position + Vector2(0, -tileSize));
+        break;
+      case 3:
+        _move(_body.first.position + Vector2(-tileSize, 0));
+        break;
+      default:
+    }
+  }
+
+  void moveRight() => _moveTo(++direction);
+  void moveStraight() => _moveTo(direction);
+  void moveLeft() => _moveTo(--direction);
 
   List<PositionComponent> get body => _body;
 }
