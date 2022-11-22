2c55f7b3921d0f3d116faedb23426cf9aac481df
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2022-11-22 18:33:57 +0100

    Add of orchestrator

---
+++ lib/services/game/game_orchestrator.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+import 'package:multiplayersnake/game/game.dart';
+import 'package:multiplayersnake/services/game/blocs/game_bloc.dart';
+import 'package:multiplayersnake/services/game/game_provider.dart';
+import 'package:multiplayersnake/services/game/game_service.dart';
+
+class GameOrchestrator implements GameProvider {
+  GameService gameService;
+
+  GameOrchestrator() : gameService = GameService();
+
+  Future<void> newGame(Rect screen, GameBloc gameBloc) async =>
+      gameService.newGame(screen, gameBloc);
+
+  MultiplayerSnakeGame? get game => gameService.game;
+
+  @override
+  Future<void> get loaded async => gameService.loaded;
+  @override
+  Future<void> get mounted async => gameService.mounted;
+  @override
+  Future<void> get ended async => gameService.ended;
+  @override
+  Future<void> get ending async => gameService.ending;
+}
+
+class GameGeneralException implements Exception {}
+
+class GameNotIstantietedException implements Exception {}
