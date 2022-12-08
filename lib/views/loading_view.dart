1d18a941e5343c7affcaa26d8b4ff32ed67427bf
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2022-12-08 17:09:32 +0100

    Add all the game settings for create

--- lib/views/loading_view.dart
+++ lib/views/loading_view.dart
@@ -16,7 +16,15 @@ class LoadingView extends StatefulWidget {
 
 class _LoadingViewState extends State<LoadingView> {
   bool? create;
-  int selectedIndex = 1;
+  late final GameSettings _gameSetting;
+  late final TextEditingController _name;
+
+  @override
+  void initState() {
+    _gameSetting = GameSettings();
+    _name = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,45 +71,137 @@ class _LoadingViewState extends State<LoadingView> {
           ),
           const SizedBox(height: 30),
           const Text("Max players: "),
-          const SizedBox(height: 30),
+          const SizedBox(height: 10),
           Wrap(
             crossAxisAlignment: WrapCrossAlignment.center,
-            children: List.generate(3, (index) {
-              print("selectedIndex: $selectedIndex");
-              print("index: $index");
-              return InkWell(
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                },
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1.0,
+            children: List.generate(
+              3,
+              (index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      _gameSetting.maxPlayers = index;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                      color: _gameSetting.maxPlayers == index
+                          ? Colors.white
+                          : Colors.blue,
                     ),
-                    borderRadius: BorderRadius.circular(15),
-                    color: selectedIndex == index ? Colors.white : Colors.blue,
-                  ),
-                  child: TextButton(
-                    onPressed: () {},
                     child: Center(
                       child: Text(
                         (index + 1).toString(),
                         style: TextStyle(
-                            color: (selectedIndex == index)
+                            color: (_gameSetting.maxPlayers == index)
                                 ? Colors.red
                                 : Colors.amber),
                       ),
                     ),
                   ),
-                ),
-              );
-            }),
-          )
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text("Max time: "),
+          const SizedBox(height: 10),
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: List.generate(
+              3,
+              (index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      _gameSetting.maxTime = index;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                      color: _gameSetting.maxTime == index
+                          ? Colors.white
+                          : Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        (index + 1).toString(),
+                        style: TextStyle(
+                            color: (_gameSetting.maxTime == index)
+                                ? Colors.red
+                                : Colors.amber),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text("Max points: "),
+          const SizedBox(height: 10),
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            children: List.generate(
+              3,
+              (index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      _gameSetting.maxPoints = index;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                      color: _gameSetting.maxPoints == index
+                          ? Colors.white
+                          : Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        (index + 1).toString(),
+                        style: TextStyle(
+                            color: (_gameSetting.maxPoints == index)
+                                ? Colors.red
+                                : Colors.amber),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text("Name: "),
+          const SizedBox(height: 10),
+          TextField(
+            controller: _name,
+            obscureText: false,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: "Enter name"),
+          ),
 
           // ElevatedButton(
           //   onPressed: () {
@@ -117,4 +217,30 @@ class _LoadingViewState extends State<LoadingView> {
       ),
     );
   }
+}
+
+class GameSettings {
+  int _indexPlayers;
+  int _indexTime;
+  int _indexPoints;
+  String name;
+  bool public;
+  final List<int> players = [1, 2, 3];
+  final List<int> time = [1, 2, 5];
+  final List<int> points = [100, 200, 400, 1000];
+
+  GameSettings()
+      : _indexPlayers = 0,
+        _indexTime = 0,
+        _indexPoints = 0,
+        name = '',
+        public = false;
+
+  int get maxPlayers => players[_indexPlayers];
+  int get maxTime => time[_indexTime];
+  int get maxPoints => points[_indexPoints];
+
+  set maxPlayers(index) => _indexPlayers = index;
+  set maxTime(index) => _indexTime = index;
+  set maxPoints(index) => _indexPoints = index;
 }
