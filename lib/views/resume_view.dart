d22c632625b2714a0468e8c5a32d234b54dc4b0f
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-01-07 22:22:54 +0100

    Skeleton of the resum view card

--- lib/views/resume_view.dart
+++ lib/views/resume_view.dart
@@ -17,12 +17,61 @@ class _ResumeViewState extends State<ResumeView> {
     return Scaffold(
       appBar: AppBar(title: const Text('Resume')),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              print('in resume');
-              context.read<GameBloc>().add(const GameEventEnded());
-            },
-            child: const Text('End')),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+                  const Text('Hola'),
+                  const SizedBox(height: 5),
+                  const Divider(
+                    thickness: 1,
+                  ),
+                  const SizedBox(height: 5),
+                  const Text('Rules'),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text('Max Time: 2 min'),
+                      Text('Max Points : 2 pts'),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  const Divider(
+                    thickness: 1,
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text('player 1: 10'),
+                      Text('player 2: 25'),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text('player 3: 50'),
+                      Text('player 4: 300'),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                context.read<GameBloc>().add(const GameEventEnded());
+              },
+              child: const Text('End'),
+            ),
+          ],
+        ),
       ),
     );
   }
