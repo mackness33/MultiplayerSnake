f7a15400cb750de4f4c51ac3df5d7646e47d44d6
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-01-11 17:12:33 +0100

    Add of id to the authenticated user

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,12 +5,13 @@ import 'package:supabase/supabase.dart' as supabase show User;
 class AuthUser {
   final bool isEmailVerified;
   final String email;
+  final String id;
 
-  const AuthUser(this.isEmailVerified, this.email);
+  const AuthUser(this.isEmailVerified, this.email, this.id);
 
   factory AuthUser.fromSupabase(supabase.User user) {
     final confirmationSentAt = user.confirmationSentAt?.split('.')[0];
     final createdAt = user.createdAt.split('.')[0];
-    return AuthUser(confirmationSentAt == createdAt, user.email ?? '');
+    return AuthUser(confirmationSentAt == createdAt, user.email ?? '', user.id);
   }
 }
